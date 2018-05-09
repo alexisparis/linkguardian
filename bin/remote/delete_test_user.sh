d41deed019a7ca39aeb69adae590efe58d11e916
Alexis PARIS <alexis.paris@blackdog-project.org>
2018-05-08 23:15:14 -0400

    delete also alexis.paris@bnc.ca from test user change mail configuration for prod and inte

--- bin/remote/delete_test_user.sh
+++ bin/remote/delete_test_user.sh
@@ -13,3 +13,10 @@ psql -d linkguardian -c "DELETE FROM link where user_id IN (select id FROM jhi_u
 psql -d linkguardian -c "DELETE FROM jhi_user_authority where user_id IN (select id FROM jhi_user where email like '$mail');"
 psql -d linkguardian -c "DELETE FROM jhi_persistent_token where user_id IN (select id FROM jhi_user where email like '$mail');"
 psql -d linkguardian -c "DELETE FROM jhi_user where email like '$mail';"
+
+mail="alexis.paris@bnc.ca"
+psql -d linkguardian -c "DELETE FROM link_tag where links_id IN  (select id FROM link where user_id IN (select id FROM jhi_user where email like '$mail'));"
+psql -d linkguardian -c "DELETE FROM link where user_id IN (select id FROM jhi_user where email like '$mail');"
+psql -d linkguardian -c "DELETE FROM jhi_user_authority where user_id IN (select id FROM jhi_user where email like '$mail');"
+psql -d linkguardian -c "DELETE FROM jhi_persistent_token where user_id IN (select id FROM jhi_user where email like '$mail');"
+psql -d linkguardian -c "DELETE FROM jhi_user where email like '$mail';"
--- src/main/webapp/app/app.constants.js
+++ src/main/webapp/app/app.constants.js
@@ -4,10 +4,10 @@
     angular
         .module('linkguardianApp')
         .constant('VERSION', "0.0.1-SNAPSHOT")
-        .constant('DEBUG_INFO_ENABLED', false)
-        .constant('BUILD_TIMESTAMP', 1525815305390)
+        .constant('DEBUG_INFO_ENABLED', true)
+        .constant('BUILD_TIMESTAMP', "")
         .constant('TEMPLATES_PATH', "app/parts/")
-        .constant('WEBSOCKET_ACTIVATED', false)
-        .constant('ENV', "prod")
+        .constant('WEBSOCKET_ACTIVATED', true)
+        .constant('ENV', "dev")
 ;
 })();
